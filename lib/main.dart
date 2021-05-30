08c2631f25b956da53d5439b2568b5cc0b272c27
Shoaib2545 <md.shoaib2545@gmail.com>
2021-05-30 17:44:43 +0500

    2nd assignment

--- lib/main.dart
+++ lib/main.dart
@@ -7,12 +7,42 @@ void main() {
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(home: Scaffold( 
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Scaffold( 
       appBar: AppBar(
-      title: Text('Assignment # 01'),
+      title: Text('Assignment # 02'),
+      backgroundColor: Color(0xffff5500),
     ),
-    body: Text('Name: Muhammad Shoaib \nGmail: md.shoaib2545@gmail.com'),
+    body: Container(
+      constraints: BoxConstraints.expand(),
+      decoration: BoxDecoration(
+        image: DecorationImage(
+          image: AssetImage("assets/images/waves_background.png"),
+          fit: BoxFit.cover)),
+          child: Stack(
+            children: [
+              Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: [
+                  Text(
+                    'Login',
+                    style: TextStyle(
+                      color: Color(0xffff5500),
+                      fontSize: 20.0,
+                      fontWeight: FontWeight.bold
+                    ),
+                  ),
+                  SizedBox(
+                    height: 50,
+                  ),
+                ],
+              ),
+            ], 
     ),
+    ),
+    )
     );
   }
 }
